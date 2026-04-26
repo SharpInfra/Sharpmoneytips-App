c1f3a091f4d9c54039f31988e90661be4623848c
SharpMoneyTips <dev@sharpmoneytips.com>
2026-04-26 03:44:38 +0000

    feat(platform): export runtime decision and tenant service modules

--- src/services/index.ts
+++ src/services/index.ts
@@ -4,3 +4,10 @@
 
 export { apiClient, ApiClient } from './apiClient';
 export { StorageService, STORAGE_KEYS } from './storage';
+export { runtimeEngine, RuntimeEngine } from './runtimeEngine';
+export { runtimeConfigService } from './runtimeConfigService';
+export { uiConfigService } from './uiConfigService';
+export { uiFeedbackService } from './uiFeedbackService';
+export { decisionMetricsService } from './decisionMetricsService';
+export { decisionApiService } from './decisionApiService';
+export { usageTrackingService } from './usageTrackingService';
--- src/store/index.ts
+++ src/store/index.ts
@@ -5,3 +5,6 @@
 export { useAuthStore } from './authStore';
 export { useAppStore } from './appStore';
 export { useThemeStore } from './themeStore';
+export { useTipsStore } from './tipsStore';
+export { useFeatureFlagsStore } from './featureFlagsStore';
+export { useUIConfigStore } from './uiConfigStore';
